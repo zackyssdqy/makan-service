56d7082b017ac4810cac3c83f8acde213a78774c
zackyssdqy <zaassetengah@gmail.com>
2025-11-26 13:02:50 +0700

    port

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 80;
+const port = 3000;
 
-// serve index.html
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
